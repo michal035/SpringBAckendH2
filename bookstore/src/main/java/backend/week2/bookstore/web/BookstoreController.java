3453c52ed06adafbdc7fd9516420efb7b9c81302
Michal <mwiselka035@gmail.com>
2024-03-14 08:38:31 +0200

    initial commit

---
+++ bookstore/src/main/java/backend/week2/bookstore/web/BookstoreController.java
@@ -0,0 +1,25 @@
+package backend.week2.bookstore.web;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@RequestMapping("/index")
+public class BookstoreController {
+    
+    @GetMapping
+    public String test(Model model) {
+        return "test";
+    }
+}
